0f283a6a0ad3183c58abcd187795d1ecb4bee89c
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-11 17:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+// Get the counter element and button element by their ids
+var counterElement = document.getElementById("counter");
+var incrementBtn = document.getElementById("incrementBtn");
+
+// Initialize the counter value
+var counterValue = 0;
+
+// Add a click event listener to the button
+incrementBtn.addEventListener("click", function() {
+    // Display an alert with the current counter value before incrementing
+    alert("Un-incremented Value: " + counterValue);
+
+    // Increment the counter value
+    counterValue++;
+
+    // Update the counter element with the new value
+    counterElement.textContent = counterValue;
+});
